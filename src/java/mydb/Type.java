ed9505b2500ed3ae06437c11166617aa987b4474
oldzhangx <zhangsirius95@gmail.com>
2019-11-23 11:22:44 -0500

    fix type outstrean

--- src/java/mydb/Type.java
+++ src/java/mydb/Type.java
@@ -51,37 +51,27 @@ public enum Type implements Serializable {
         public Field parse(DataInputStream dataInputStream) throws ParseException {
 
             try{
-                int length = dataInputStream.readInt()
-            }
+                // read first four is string's length
+                int length = dataInputStream.readInt();
+                byte[] bytes = new byte[length];
 
+                dataInputStream.read(bytes);
+                dataInputStream.skipBytes(STRING_LEN -length);
 
-            try {
-                int strLen = dis.readInt();
-                byte[] bs = new byte[strLen];
-
-                dis.read(bs);
-                dis.skipBytes(STRING_LEN - strLen);
-                return new StringField( new String(bs), STRING_LEN);
+                return new StringField(new String(bytes), STRING_LEN);
             } catch (IOException e) {
-                throw new ParseException("parse error", 0);
+                e.printStackTrace();
+                throw new ParseException("String parse error", 0);
             }
+
         }
     };
     
     public static final int STRING_LEN = 128;
 
-  /**
-   * @return the number of bytes required to store a field of this type.
-   */
     public abstract int getLen();
 
-  /**
-   * @return a Field object of the same type as this object that has contents
-   *   read from the specified DataInputStream.
-   * @param dis The input stream to read from
-   * @throws ParseException if the data read from the input stream is not
-   *   of the appropriate type.
-   */
+    // read
     public abstract Field parse(DataInputStream dis) throws ParseException;
 
 }
