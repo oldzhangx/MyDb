cf38f366a78e52d946505f53020a31d3b05b60ef
oldzhangx <zhangsirius95@gmail.com>
2019-11-25 13:43:24 -0500

    fix tuple null

--- src/java/mydb/TupleDetail/TupleDetail.java
+++ src/java/mydb/TupleDetail/TupleDetail.java
@@ -12,7 +12,7 @@ public class TupleDetail implements Serializable {
     private static final long serialVersionUID = 7041402579811379073L;
 
     //length of tuplecells
-    private Integer fieldNumber;
+//    private Integer fieldNumber;
 
     private TupleCell[] tupleCells;
 
@@ -40,9 +40,8 @@ public class TupleDetail implements Serializable {
     public TupleDetail(TupleCell[] tupleCells) {
         if(tupleCells == null) throw new IllegalArgumentException("tdItems null error");
         if(tupleCells.length == 0) throw new IllegalArgumentException("tdItems length 0 tuple error");
-
-        fieldNumber = tupleCells.length;
         this.tupleCells = tupleCells;
+
     }
     public TupleDetail(Type[] typeAr) {
         this(typeAr, new String[typeAr.length]);
@@ -64,50 +63,30 @@ public class TupleDetail implements Serializable {
         if(typeAr.length == 0) throw new IllegalArgumentException("typeAr length 0 tuple error");
         if(typeAr.length != fieldAr.length) throw new IllegalArgumentException("typeAr length has to be equal to fieldAr");
 
-        fieldNumber = typeAr.length;
         tupleCells = new TupleCell[typeAr.length];
 
-        for(int i = 0; i < fieldNumber; i++) {
+        for(int i = 0; i < typeAr.length; i++) {
             tupleCells[i] = new TupleCell(typeAr[i], fieldAr[i]);
         }
     }
 
-    /**
-     * @return the number of fields in this TupleDesc
-     */
     public int fieldNumber() {
-        return this.fieldNumber;
+        return tupleCells.length;
     }
 
-    /**
-     * Gets the (possibly null) field name of the ith field of this TupleDesc.
-     * 
-     * @param i
-     *            index of the field name to return. It must be a valid index.
-     * @return the name of the ith field
-     * @throws NoSuchElementException
-     *             if i is not a valid field reference.
-     */
     public String getFieldName(int i) throws NoSuchElementException {
-        if (i < 0 || i> fieldNumber) throw new NoSuchElementException();
+        if(tupleCells == null) throw new NoSuchElementException();
+        if (i < 0 || i> tupleCells.length) throw new NoSuchElementException();
         return tupleCells[i].fieldName;
     }
 
 
     public Type getFieldType(int i) throws NoSuchElementException {
-        if (i < 0 || i> this.fieldNumber) throw new NoSuchElementException();
+        if(tupleCells == null) throw new NoSuchElementException();
+        if (i < 0 || i> this.tupleCells.length) throw new NoSuchElementException();
         return tupleCells[i].fieldType;
     }
 
-    /**
-     * Find the index of the field with a given name.
-     * 
-     * @param name
-     *            name of the field.
-     * @return the index of the field that is first to have the given name.
-     * @throws NoSuchElementException
-     *             if no field with a matching name is found.
-     */
     public int fieldNameToIndex(String name) throws NoSuchElementException {
         if(name == null) throw new NoSuchElementException();
         for(int i = 0; i< tupleCells.length; i++){
@@ -117,10 +96,6 @@ public class TupleDetail implements Serializable {
         throw new NoSuchElementException();
     }
 
-    /**
-     * @return The size (in bytes) of tuples corresponding to this TupleDesc.
-     *         Note that tuples from a given TupleDesc are of a fixed size.
-     */
     public int getSize() {
         int totalSize = 0;
         for (TupleCell tupleCell : tupleCells) {
@@ -129,50 +104,26 @@ public class TupleDetail implements Serializable {
         return totalSize;
     }
 
-    /**
-     * Merge two TupleDescs into one, with td1.numFields + td2.numFields fields,
-     * with the first td1.numFields coming from td1 and the remaining from td2.
-     * 
-     * @param td1
-     *            The TupleDesc with the first fields of the new TupleDesc
-     * @param td2
-     *            The TupleDesc with the last fields of the TupleDesc
-     * @return the new TupleDesc
-     */
     public static TupleDetail merge(TupleDetail td1, TupleDetail td2) {
         if(td1 == null) return td2;
         if(td2 == null) return td1;
         TupleCell[] tupleCells = new TupleCell[td1.tupleCells.length+ td2.tupleCells.length];
-        for(int i = 0; i< td1.tupleCells.length; i++){
-            tupleCells[i] = td1.tupleCells[i];
-        }
-        for(int i = 0; i< td2.tupleCells.length; i++){
-            tupleCells[i+ td1.tupleCells.length] = td2.tupleCells[i];
-        }
+        System.arraycopy(td1.tupleCells, 0, tupleCells, 0, td1.tupleCells.length);
+        System.arraycopy(td2.tupleCells, 0, tupleCells, td1.tupleCells.length, td2.tupleCells.length);
         return new TupleDetail(tupleCells);
     }
 
-    /**
-     * Compares the specified object with this TupleDesc for equality. Two
-     * TupleDescs are considered equal if they are the same size and if the n-th
-     * type in this TupleDesc is equal to the n-th type in td.
-     * 
-     * @param o
-     *            the Object to be compared for equality with this TupleDesc.
-     * @return true if the object is equal to this TupleDesc.
-     */
+
     public boolean equals(Object o) {
         if(o == null) return false;
         if(o == this) return true;
-        if(o.getClass() == this.getClass()){
-            TupleDetail tupleDetail = (TupleDetail) o;
-            if (!tupleDetail.fieldNumber.equals(this.fieldNumber)) return false;
-            for(int i = 0; i< tupleDetail.fieldNumber; i++)
-                if(!tupleDetail.tupleCells[i].equals(this.tupleCells[i]))
-                    return false;
+        if(o.getClass() != this.getClass()) return false;
+        TupleDetail tupleDetail = (TupleDetail) o;
+        if(tupleDetail.tupleCells.length != tupleCells.length) return false;
+        for(int i = 0; i< tupleCells.length ; i++)
+            if(!tupleCells[i].equals(tupleDetail.tupleCells[i]))
+                return false;
             return true;
-        }
-        return false;
     }
 
     public int hashCode() {
@@ -181,13 +132,6 @@ public class TupleDetail implements Serializable {
         throw new UnsupportedOperationException("unimplemented");
     }
 
-    /**
-     * Returns a String describing this descriptor. It should be of the form
-     * "fieldType[0](fieldName[0]), ..., fieldType[M](fieldName[M])", although
-     * the exact format does not matter.
-     * 
-     * @return String describing this descriptor.
-     */
     public String toString() {
         TupleCell[] tupleCells = this.tupleCells;
         StringBuilder result = new StringBuilder();
