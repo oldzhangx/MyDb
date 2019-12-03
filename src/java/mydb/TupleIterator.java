3436336c6a1b7f49a4934b364cd752e9ee6dd922
oldzhangx <zhangsirius95@gmail.com>
2019-12-02 22:48:25 -0500

    convert tupleiterator

--- src/java/mydb/TupleIterator.java
+++ src/java/mydb/TupleIterator.java
@@ -2,37 +2,22 @@ package mydb;
 
 import mydb.TupleDetail.Tuple;
 import mydb.TupleDetail.TupleDetail;
-
 import java.util.Iterator;
 
-
-/**
- * Implements a DbIterator by wrapping an Iterable<Tuple>.
- */
 public class TupleIterator implements DbIterator {
     private static final long serialVersionUID = -1020449676759920107L;
 
     private Iterator<Tuple> i = null;
-    private TupleDetail td = null;
+    private TupleDetail tupleDetail = null;
     private Iterable<Tuple> tuples = null;
 
-    /**
-     * Constructs an iterator from the specified Iterable, and the specified
-     * descriptor.
-     * 
-     * @param tuples
-     *            The set of tuples to iterate over
-     */
-    public TupleIterator(TupleDetail td, Iterable<Tuple> tuples) {
-        this.td = td;
+    public TupleIterator(TupleDetail tupleDetail, Iterable<Tuple> tuples) {
+        this.tupleDetail = tupleDetail;
         this.tuples = tuples;
 
-        // check that all tuples are the right TupleDesc
-        for (Tuple t : tuples) {
-            if (!t.getTupleDetail().equals(td))
-                throw new IllegalArgumentException(
-                        "incompatible tuple in tuple set");
-        }
+        for (Tuple t : tuples)
+            if (!t.getTupleDetail().equals(tupleDetail))
+                throw new IllegalArgumentException("incompatible tuple in tuple set");
     }
 
     public void open() {
@@ -53,7 +38,7 @@ public class TupleIterator implements DbIterator {
     }
 
     public TupleDetail getTupleDetail() {
-        return td;
+        return tupleDetail;
     }
 
     public void close() {
